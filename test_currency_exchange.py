f41e47f8027a2b720af13bb8a127f3c457feff30
Ivan Zhilnikov <feliksesae@gmail.com>
2023-08-11 22:38:44 +0400

    minor fix

--- test_currency_exchange.py
+++ test_currency_exchange.py
@@ -3,7 +3,7 @@ from requests import Response
 
 from unittest.mock import Mock, patch
 
-from src.currency_exchange import currency_exchange
+from currency_exchange import currency_exchange
 
 
 # Позитивные тесты
