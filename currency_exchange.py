a498bd582a237e55e5b7454eabcee957e80ca783
Ivan Zhilnikov <feliksesae@gmail.com>
2023-08-11 13:10:09 +0400

    Deployed the project

--- currency_exchange.py
+++ currency_exchange.py
@@ -1,1 +1,73 @@
-13
+import requests
+
+
+def currency_exchange(base: str = 'USD',
+                      symbols: str = 'EUR',
+                      amount: float = 1.0,
+                      places: int = 2,
+                      source: str = 'ecb') -> float:
+    """
+    Функция, выполняющая конвертации пары валют.
+
+    Args:
+        base (str): Базовая валюта. Должен быть одним из ['USD', 'EUR', 'JPY'].
+        symbols (str): Конечная валюта. Должен быть одним из ['USD', 'EUR', 'JPY'].
+        amount (float): Сумма конвертации.
+        places (int): Округление, количество знаков после запятой.
+        source (str): Банк, источника данных. Должен быть одним из ['ecb', 'cbr', 'imf'].
+            :Возможные значения для параметра `source`(https://api.exchangerate.host/sources):
+            - 'ecb': Центральный банк Европы. Нет возможности конвертировать рубли.
+            - 'cbr': Центральный Банк России. Не возвращает данные (работает по xml).
+            - 'imf': Международный валютный фонд. Все ОК.
+
+    Returns:
+        float: Результат конвертации валют
+
+    Raises:
+        ValueError: Если параметр `base` или `symbols` не является строкой.
+        ValueError: Если параметр `amount` не является числом, меньше или равен нулю, имеет более 20 знаков.
+        ValueError: Если параметр `places` не является целым числом, меньше нуля, имеет более 5 знаков.
+        ValueError: Если параметр `source` не принадлежит к ['ecb', 'cbr', 'imf'].
+        ValueError: Если ответ от API не содержит данные rates или не является числом.
+
+    """
+    if not isinstance(base, str):
+        raise ValueError("Parameter 'base' must be a string.")
+    if base not in ['USD', 'EUR', 'JPY']:
+        raise ValueError("Parameter 'base' must be one of ['USD', 'EUR', 'JPY'].")
+    if not isinstance(symbols, str):
+        raise ValueError("Parameter 'symbols' must be a string.")
+    if symbols not in ['USD', 'EUR', 'JPY']:
+        raise ValueError("Parameter 'symbols' must be one of ['USD', 'EUR', 'JPY'].")
+    if not isinstance(amount, (int, float)):
+        raise ValueError("Parameter 'amount' must be a number.")
+    if amount <= 0:
+        raise ValueError("Parameter 'amount' must be a positive number.")
+    if len(str(amount)) > 20:
+        raise ValueError("Parameter 'amount' must have at most 20 digits.")
+    if not isinstance(places, int):
+        raise ValueError("Parameter 'places' must be an integer.")
+    if places <= 0:
+        raise ValueError("Parameter 'places' must be a positive number.")
+    if len(str(places)) > 5:
+        raise ValueError("Parameter 'places' must have at most 5 digits.")
+    if source not in ['ecb', 'cbr', 'imf']:
+        raise ValueError("Parameter 'source' must be one of ['ecb', 'cbr', 'imf'].")
+
+    url = 'https://api.exchangerate.host/latest'
+    query_parameters = {'base': base, 'symbols': symbols, 'amount': amount, 'places': places, 'source': source}
+    response = requests.get(url, params=query_parameters)
+
+    # Проверка статуса ответа от API
+    if response.status_code != 200:
+        raise ValueError(f"Invalid response from API: Status code {response.status_code}")
+
+    data = response.json()
+
+    # Проверка ответа от API
+    if not data.get('rates') or not isinstance(data['rates'].get(symbols), (int, float)):
+        raise ValueError("Invalid response from API: rates not found or not a number.")
+
+    result = data['rates'][symbols]
+
+    return result
--- test_currency_exchange.py
+++ test_currency_exchange.py
@@ -1,1 +1,68 @@
-123
+import pytest
+from requests import Response
+
+from unittest.mock import Mock, patch
+
+from currency_exchange import currency_exchange
+
+
+# Позитивные тесты
+def test_currency_exchange_positive():
+    # Проверяем успешную конвертацию с USD в EUR с базовой суммой 1
+    result = currency_exchange(base='USD', symbols='EUR')
+    assert isinstance(result, float)
+
+
+# Негативные тесты
+def test_currency_exchange_negative_base_value_error():
+    with pytest.raises(ValueError):
+        currency_exchange(base='RUB')  # Некорректный базовый параметр
+
+
+def test_currency_exchange_negative_symbols_value_error():
+    with pytest.raises(ValueError):
+        currency_exchange(symbols='RUB')  # Некорректный базовый параметр
+
+
+def test_currency_exchange_negative_amount_negative_number():
+    with pytest.raises(ValueError):
+        currency_exchange(amount=-10)  # Отрицательная базовая сумма
+
+
+def test_currency_exchange_negative_amount_long_number():
+    with pytest.raises(ValueError):
+        currency_exchange(amount=123456789012345678901)  # Базовая сумма слишком большая
+
+
+def test_currency_exchange_negative_places_negative_number():
+    with pytest.raises(ValueError):
+        currency_exchange(places=-1)  # Отрицательное округление
+
+
+def test_currency_exchange_negative_places_long_number():
+    with pytest.raises(ValueError):
+        currency_exchange(places=1234567)  # Слишком большое округление
+
+
+def test_currency_exchange_negative_source_wrong():
+    with pytest.raises(ValueError):
+        currency_exchange(source='wrong_source')  # Некорректный источник
+
+
+# Негативный тест для проверки статуса ответа от API
+def test_currency_exchange_api_status_negative():
+    # Создаем мок объект для имитации ответа от API с некорректным статусом
+    response_mock = Mock(spec=Response)
+    response_mock.status_code = 404  # Устанавливаем статус код 404
+
+    # Имитируем запрос к API с помощью патча
+    with patch('requests.get', return_value=response_mock) as mock_request:
+        # Проверяем, что функция вызовет ValueError с соответствующим сообщением
+        with pytest.raises(ValueError, match=r"Invalid response from API: Status code 404"):
+            currency_exchange(base='USD', symbols='EUR', amount=100)
+
+        # Проверяем, что запрос к API был выполнен с нужными параметрами
+        mock_request.assert_called_once_with(
+            'https://api.exchangerate.host/latest',
+            params={'base': 'USD', 'symbols': 'EUR', 'amount': 100, 'places': 2, 'source': 'ecb'}
+        )
